925a2148253783e180d32a46e82261a96ec55996
Elena Georgieva <elena@phyreapp.com>
2021-05-22 11:54:42 +0300

    getting started

--- Messenger/Messenger/Controllers/ConversationsViewController.swift
+++ Messenger/Messenger/Controllers/ConversationsViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  ConversationsViewController.swift
 //  Messenger
 //
 //  Created by Elena Georgieva on 22.05.21.
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ConversationsViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
