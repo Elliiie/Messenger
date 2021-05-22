be10e04fb5f5603855c75f15c1031d87d524e6e4
Elena Georgieva <elena@phyreapp.com>
2021-05-22 12:57:30 +0300

    implement login UI

---
+++ Messenger/Messenger/Controllers/Login/LoginViewController.swift
@@ -0,0 +1,124 @@
+//
+//  LoginViewController.swift
+//  Messenger
+//
+//  Created by Elena Georgieva on 22.05.21.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    private let imageView = UIImageView()
+    private let scrollView = UIScrollView()
+    private let emailField = UITextField()
+    private let passwordField = UITextField()
+    private let loginButton = UIButton()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        title = "Login"
+        view.backgroundColor = .white
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Register", style: .done, target: self, action: #selector(didTapRegister))
+        
+        scrollView.clipsToBounds = true
+        view.addSubview(scrollView)
+        
+        imageView.image = UIImage(named: "logo")
+        imageView.contentMode = .scaleAspectFit
+        scrollView.addSubview(imageView)
+        
+        emailField.delegate = self
+        emailField.autocapitalizationType = .none
+        emailField.autocorrectionType = .no
+        emailField.returnKeyType = .continue
+        emailField.layer.cornerRadius = 12
+        emailField.layer.borderWidth = 2
+        emailField.layer.borderColor = UIColor.lightGray.cgColor
+        emailField.placeholder = "Email address..."
+        emailField.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 5, height: 0))
+        
+        emailField.leftViewMode = .always
+        emailField.backgroundColor = .white
+        scrollView.addSubview(emailField)
+        
+        passwordField.delegate = self
+        passwordField.autocapitalizationType = .none
+        passwordField.autocorrectionType = .no
+        passwordField.returnKeyType = .done
+        passwordField.layer.cornerRadius = 12
+        passwordField.layer.borderWidth = 2
+        passwordField.layer.borderColor = UIColor.lightGray.cgColor
+        passwordField.placeholder = "Password..."
+        passwordField.isSecureTextEntry = true
+        passwordField.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 5, height: 0))
+        
+        passwordField.leftViewMode = .always
+        passwordField.backgroundColor = .white
+        scrollView.addSubview(passwordField)
+        
+        loginButton.setTitle("Login", for: .normal)
+        loginButton.backgroundColor = .link
+        loginButton.setTitleColor(.white, for: .normal)
+        loginButton.layer.cornerRadius = 12
+        loginButton.layer.masksToBounds = true
+        loginButton.titleLabel?.font = .systemFont(ofSize: 20, weight: .bold)
+        loginButton.addTarget(self, action: #selector(loginButtonTapped), for: .touchUpInside)
+        scrollView.addSubview(loginButton)
+    }
+
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        scrollView.frame = view.bounds
+
+        let size = scrollView.width/3
+        imageView.frame = CGRect(x: (scrollView.width - size)/2, y: 20, width: size, height: size)
+        
+        emailField.frame = CGRect(x: 30, y: imageView.bottom + 10, width: scrollView.width - 60, height: 52)
+        
+        passwordField.frame = CGRect(x: 30, y: emailField.bottom + 10, width: scrollView.width - 60, height: 52)
+        
+        loginButton.frame = CGRect(x: 30, y: passwordField.bottom + 10, width: scrollView.width - 60, height: 52)
+
+
+    }
+    
+    private func allertUserLogginError() {
+        let alert = UIAlertController(title: "Woops", message: "Please, enter all information to login", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Dismiss", style: .cancel, handler: nil))
+        
+        present(alert, animated: true, completion: nil)
+    }
+    
+    @objc private func loginButtonTapped() {
+        guard let email = emailField.text, let password = passwordField.text, !email.isEmpty, !password.isEmpty, password.count >= 6 else {
+            allertUserLogginError()
+            return
+        }
+        
+        //TODO:- login
+    }
+    
+    @objc private func didTapRegister() {
+        let vc = RegisterViewController()
+        vc.title = "Create account"
+        
+        navigationController?.pushViewController(vc, animated: true)
+    }
+}
+
+extension LoginViewController: UITextFieldDelegate {
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if textField == emailField {
+            passwordField.becomeFirstResponder()
+        } else if textField == passwordField {
+            loginButtonTapped()
+        }
+        
+        return true
+    }
+}
---
+++ Messenger/Messenger/Controllers/Login/RegisterViewController.swift
@@ -0,0 +1,18 @@
+//
+//  RegisterViewController.swift
+//  Messenger
+//
+//  Created by Elena Georgieva on 22.05.21.
+//
+
+import UIKit
+
+class RegisterViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .link
+    }
+
+}
